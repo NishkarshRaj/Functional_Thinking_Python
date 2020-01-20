9dff8ab39074657a4f6277f4c2377fa6e2749424
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-01-20 18:27:24 +0530

    Create OneFlip.py

---
+++ Assignment1/OneFlip.py
@@ -0,0 +1,12 @@
+# Take a numeric input in string format
+var = input('Enter a number: ')
+
+# Declare list
+list1 = list()
+
+# Convert string into list of characters
+for el in var:
+    list1.append(el)
+    
+# Display the list
+print(list1)
