f9d89639e6d7f82f15b777dafbc586cf80c74856
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-02-02 18:34:08 +0530

    Update BubbleSort_Iterative.py

--- Searching/BubbleSort_Iterative.py
+++ Searching/BubbleSort_Iterative.py
@@ -9,7 +9,9 @@ print("Enter Array as space separated values: ")
 array = list(map(int,input().split(' ')))
 
 for i in range(len(array)):
-    for j in range(len(array)-1):
+    # for j in range(len(array)-1): While this works, the last ith elements are sorted 
+    '''For each iteration, the largest element goes at the last part'''
+    for j in range(len(array)-1-i):
         if(array[j+1]<array[j]):
             array[j], array[j+1] = array[j+1], array[j] 
     
