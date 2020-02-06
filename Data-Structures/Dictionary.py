e87ce3d241effadce5389dd3499287986b20fb22
Nishkarsh Raj <500060720@stu.upes.ac.in>
2020-02-07 05:17:04 +0530

    Create Dictionary.py

---
+++ Data-Structures/Dictionary.py
@@ -0,0 +1,10 @@
+d = dict()
+print(d) # {}
+s = set()
+print(s) # set()
+
+d = {"name":"Nishkarsh Raj","age":21,"Branch":"DevOps"}
+print(d)
+print(d.keys())
+print(d.values())
+print(d.items()) # Tuples of key value pairs
